c0f7daced15789fce8e44ce55ca820b294ccb672
Tim Jarratt <pair+tjarratt@gmail.com>
2016-06-06 00:37:42 -0700

    Remove tab bar controller

--- StreamCenter/AppDelegate.swift
+++ StreamCenter/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         let window = UIWindow.init(frame: UIScreen.mainScreen().bounds)
-        window.rootViewController = SourceTabController()
+        window.rootViewController = TwitchGamesViewController()
         window.makeKeyAndVisible()
         self.window = window
         
